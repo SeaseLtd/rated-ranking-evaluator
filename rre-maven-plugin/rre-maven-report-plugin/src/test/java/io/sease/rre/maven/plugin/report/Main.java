37e14eb8845c76b0339cd946f8ff5eed6ee88c75
agazzarini <a.gazzarini@gmail.com>
2018-05-25 22:35:33 +0200

    [ issue #2 ] First draft

---
+++ rre-maven-plugin/rre-maven-report-plugin/src/test/java/io/sease/rre/maven/plugin/report/Main.java
@@ -0,0 +1,19 @@
+package io.sease.rre.maven.plugin.report;
+
+import org.apache.maven.reporting.MavenReportException;
+
+import java.io.File;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String a []) throws Exception {
+        RREReport report = new RREReport(){
+            @Override
+            File evaluationOutputFile() throws MavenReportException {
+                return new File("/Users/agazzarini/IdeaProjects/rated-ranking-evaluator/rre-maven-plugin/rre-maven-solr-plugin/target/rre/evaluation.json");
+            }
+        };
+
+        report.executeReport(Locale.getDefault());
+    }
+}
