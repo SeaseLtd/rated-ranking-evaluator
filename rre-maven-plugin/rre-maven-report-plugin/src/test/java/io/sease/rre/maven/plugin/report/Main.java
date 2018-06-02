7035ab9bc0ba8e5d8f2258e9367723535c863a94
agazzarini <a.gazzarini@spaziocodice.com>
2018-06-02 12:29:34 +0200

    [ issue #13 ] report with revisited datamodel (doesn't compile)

--- rre-maven-plugin/rre-maven-report-plugin/src/test/java/io/sease/rre/maven/plugin/report/Main.java
+++ rre-maven-plugin/rre-maven-report-plugin/src/test/java/io/sease/rre/maven/plugin/report/Main.java
@@ -3,16 +3,27 @@ package io.sease.rre.maven.plugin.report;
 import org.apache.maven.reporting.MavenReportException;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.Locale;
+
+import static java.util.Arrays.asList;
 
 public class Main {
     public static void main(String a []) throws Exception {
         RREReport report = new RREReport(){
             @Override
             File evaluationOutputFile() {
-                return new File("/Users/agazzarini/IdeaProjects/rated-ranking-evaluator/rre-maven-plugin/rre-maven-solr-plugin/target/rre/evaluation.json");
+                return new File("/Users/agazzarini/workspaces/rated-ranking-evaluator/rre-maven-plugin/rre-maven-solr-plugin/target/rre/evaluation.json");
+            }
+
+            @Override
+            public String getOutputName() {
+                return "rre-report";
             }
         };
+
+        report.setReportOutputDirectory(new File("/Users/agazzarini/workspaces/rated-ranking-evaluator/rre-maven-plugin/rre-maven-solr-plugin/target/rre"));
+        report.formats = Collections.singletonList("spreadsheet");
 
         report.executeReport(Locale.getDefault());
     }
