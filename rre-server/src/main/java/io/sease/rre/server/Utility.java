84a58a2f87bef60c70ad4077892cb0a5ee15c73b
agazzarini <a.gazzarini@spaziocodice.com>
2018-06-06 13:01:05 +0200

    [ issue #17 ] Working RRE Server

--- rre-server/src/main/java/io/sease/rre/server/Utility.java
+++ rre-server/src/main/java/io/sease/rre/server/Utility.java
@@ -3,21 +3,29 @@ package io.sease.rre.server;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.math.BigDecimal;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+/**
+ * Shared utilities.
+ *
+ * @author agazzarini
+ * @since 1.0
+ */
 public abstract class Utility {
-
-    public static String pretty(final JsonNode node) {
+    /**
+     * Returns the input JSON payload as a pretty formatted string.
+     *
+     * @param payload the JSON object.
+     * @return the input JSON payload as a pretty formatted string.
+     */
+    public static String pretty(final JsonNode payload) {
         try {
             final ObjectMapper mapper = new ObjectMapper();
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(mapper.readTree(node.asText()));
-        } catch (Exception exception) {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(mapper.readTree(payload.asText()));
+        } catch (final Exception exception) {
             throw new RuntimeException(exception);
         }
-    }
-
-    public static Stream<JsonNode> all(final JsonNode parent, final String name) {
-        return StreamSupport.stream(parent.get(name).spliterator(), false);
     }
 }
