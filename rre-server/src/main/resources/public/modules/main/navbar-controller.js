dbebeb15d2130e49b01bacdacdcbd94838b94b73
agazzarini <a.gazzarini@spaziocodice.com>
2018-06-15 14:58:52 +0200

    [ issue #17 ] AngularJS console (v1.0)

---
+++ rre-server/src/main/resources/public/modules/main/navbar-controller.js
@@ -0,0 +1,31 @@
+(function () {
+    angular.module('myApp').controller('NavbarController', NavbarController);
+
+    NavbarController.$inject = ['$scope', '$http', '$log', 'DataService'];
+    function NavbarController($scope, $http, $log, DataService) {
+        var vm = this;
+
+        // Scope vars
+        activate();
+
+        ////////////
+
+        /**
+         * controller activation
+         */
+        function activate() {
+            $log.log('NavbarController', 'starting');
+            DataService.getData().then(
+                function (response) {
+                    vm.navBarTitle = response.data.name;
+                },
+                function (error) {
+                    $log.error("DataService - Error while performing request:", error);
+                }
+            );
+
+            $scope.vm = vm;
+        }
+
+    }
+})();
