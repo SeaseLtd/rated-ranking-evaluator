314c408020c9af82541769b7ae7f837c65799337
agazzarini <a.gazzarini@spaziocodice.com>
2018-06-15 14:58:52 +0200

    [ issue #17 ] AngularJS console (v1.0)

---
+++ rre-server/src/main/resources/public/index.js
@@ -0,0 +1,20 @@
+var myApp = angular.module("myApp", ["ngRoute"])
+        .run(function ($rootScope, $timeout) {
+
+        });
+
+
+myApp.config(function($routeProvider) {
+    $routeProvider
+            .when("/", {
+                templateUrl : "modules/main/views/dashboard.html",
+                controller: "DashboardController"
+            })
+            .when("/dashboard", {
+                templateUrl : "modules/main/views/dashboard.html",
+                controller: "DashboardController"
+            })
+            .otherwise({
+                template : "<h1>Not found</h1>"
+            });
+});
---
+++ rre-server/src/main/resources/public/modules/main/dashboard-controller.js
@@ -0,0 +1,55 @@
+(function () {
+    angular.module('myApp').controller('DashboardController', DashboardController);
+
+    DashboardController.$inject = ['$scope', '$http', '$log', 'DataService', '$interval', 'ConfigService'];
+    function DashboardController($scope, $http, $log, DataService, $interval, ConfigService) {
+        var vm = this;
+
+        // Scope vars
+        vm.data = null;
+
+        // Methods
+        vm.getMetricsCount = getMetricsCount;
+
+        activate();
+
+
+        ////////////
+
+        /**
+         * controller activation
+         */
+        function activate() {
+            DataService.getData().then(
+                function (response) {
+                    vm.data = response.data;
+                },
+                function (error) {
+                    $log.error("DataService - Error while performing request:", response);
+                }
+            );
+            $interval(function() {
+                DataService.getData().then(
+                    function (response) {
+                        vm.data = response.data;
+                    },
+                    function (error) {
+                        $log.error("DataService - Error while performing request:", error);
+                    }
+                );
+            }, ConfigService.requestInterval);
+            $scope.vm = vm;
+            $log.log('DashboardController', 'starting');
+        }
+
+
+        function getMetricsCount() {
+            if (vm.data != null) {
+                return Object.keys(vm.data.metrics).length;
+            }
+            return 0;
+        }
+
+
+    }
+})();
