6d00d9d7e6f85489b28c06b5b0f04a04cf3a4e2d
agazzarini <a.gazzarini@gmail.com>
2018-07-01 16:29:16 +0200

    [ issue #17 ] RRE Module clean up

---
+++ rre-server/src/main/resources/static/modules/main/config-service.js
@@ -0,0 +1,36 @@
+(function () {
+    angular.module('myApp').factory('ConfigService', ConfigService);
+
+    ConfigService.$inject = ['$log'];
+    function ConfigService($log, $http) {
+
+        /**
+         * Request interval in milliseconds
+         * @type {number}
+         */
+        var requestInterval = 5000;
+
+        /**
+         * The data request URL
+         * @type {string}
+         */
+        var requestUrl = "http://127.0.0.1:8080/evaluation";
+
+        init();
+
+        return {
+            requestInterval: requestInterval,
+            requestUrl: requestUrl
+        };
+
+        ////////////
+
+        /**
+         * Init
+         */
+        function init() {
+            $log.log("ConfigService", "starting");
+        }
+
+    }
+})();
