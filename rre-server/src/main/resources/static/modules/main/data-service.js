61ee4bd8cd963e1e4ccdb47baee6f58e2c135136
agazzarini <a.gazzarini@gmail.com>
2018-07-01 16:29:16 +0200

    [ issue #17 ] RRE Module clean up

---
+++ rre-server/src/main/resources/static/modules/main/data-service.js
@@ -0,0 +1,26 @@
+(function () {
+    angular.module('myApp').factory('DataService', DataService);
+
+    DataService.$inject = ['$log', '$http', 'ConfigService'];
+    function DataService($log, $http, ConfigService) {
+
+        init();
+
+        return {
+            getData: getData
+        };
+
+        ////////////
+
+        /**
+         * Init
+         */
+        function init() {
+            $log.log("DataService", "starting");
+        }
+
+        function getData() {
+            return $http.get(ConfigService.requestUrl);
+        }
+    }
+})();
