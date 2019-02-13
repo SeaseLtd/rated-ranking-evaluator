c8a93e9669b2d8916d5c1bba6ed0416755956069
Daniel Worley <danworley@gmail.com>
2019-02-12 20:33:06 -0500

    Test cases for new at K classes

---
+++ rre-core/src/test/java/io.sease.rre.core.domain.metrics.impl/PrecisionAtKTestCase.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sease.rre.core.domain.metrics.impl;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.sease.rre.core.BaseTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.sease.rre.core.TestData.*;
+import static java.util.Arrays.stream;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Precision at K Test Case.
+ *
+ * @author worleydl
+ */
+public class PrecisionAtKTestCase extends BaseTestCase {
+    /**
+     * Setup fixture for this test case.
+     */
+    @Before
+    public void setUp() {
+        cut = new PrecisionAtK(15);
+        cut.setVersions(Collections.singletonList(A_VERSION));
+        counter = new AtomicInteger(0);
+    }
+
+    /**
+     * If all results in the window are relevant, then the precision is 1.
+     */
+    @Test
+    public void maximumPrecision() {
+       maximum();
+    }
+
+    /**
+     * Scenario: 15 judgments, 15 search results, 10 relevant results in top positions.
+     */
+    @Test
+    public void _10_judgments_15_search_results_10_relevant_results_at_top() {
+        final ObjectNode judgements = mapper.createObjectNode();
+        stream(FIFTEEN_SEARCH_HITS).limit(10).forEach(docid -> judgements.set(docid, createJudgmentNode(3)));
+        cut.setRelevantDocuments(judgements);
+
+        cut.setTotalHits(FIFTEEN_SEARCH_HITS.length, A_VERSION);
+        stream(FIFTEEN_SEARCH_HITS)
+                .map(this::searchHit)
+                .forEach(hit -> cut.collect(hit, counter.incrementAndGet(), A_VERSION));
+
+        assertEquals(
+                10 / 15d,
+                cut.valueFactory(A_VERSION).value().doubleValue(),
+                0.01);
+    }
+
+    /**
+     * Scenario: 10 judgments, 15 search results, 5 relevant results in top positions.
+     */
+    @Test
+    public void _10_judgments_15_search_results_5_relevant_results() {
+        final ObjectNode judgements = mapper.createObjectNode();
+        stream(FIFTEEN_SEARCH_HITS).limit(10).forEach(docid -> judgements.set(docid, createJudgmentNode(3)));
+        cut.setRelevantDocuments(judgements);
+
+        cut.setTotalHits(FIFTEEN_SEARCH_HITS.length, A_VERSION);
+        stream(FIVE_SEARCH_HITS)
+                .map(this::searchHit)
+                .forEach(hit -> cut.collect(hit, counter.incrementAndGet(), A_VERSION));
+
+        stream(TEN_SEARCH_HITS)
+                .map(docid -> docid + "_SUFFIX")
+                .map(this::searchHit)
+                .forEach(hit -> cut.collect(hit, counter.incrementAndGet(), A_VERSION));
+
+        assertEquals(
+                5 / 15d,
+                cut.valueFactory(A_VERSION).value().doubleValue(),
+                0.01);
+    }
+
+    /**
+     * Scenario: 10 judgments, 15 search results, 5 relevant results in top positions.
+     */
+    @Test
+    public void _10_judgments_15_search_results_5_relevant_results_in_the_middle() {
+        final ObjectNode judgements = mapper.createObjectNode();
+        stream(FIFTEEN_SEARCH_HITS).limit(10).forEach(docid -> judgements.set(docid, createJudgmentNode(3)));
+        cut.setRelevantDocuments(judgements);
+
+        cut.setTotalHits(FIFTEEN_SEARCH_HITS.length, A_VERSION);
+        stream(FIVE_SEARCH_HITS)
+                .map(docid -> docid + "_PRE")
+                .map(this::searchHit)
+                .forEach(hit -> cut.collect(hit, counter.incrementAndGet(), A_VERSION));
+
+        stream(FIVE_SEARCH_HITS)
+                .map(this::searchHit)
+                .forEach(hit -> cut.collect(hit, counter.incrementAndGet(), A_VERSION));
+
+        stream(FIVE_SEARCH_HITS)
+                .map(docid -> docid + "_POST")
+                .map(this::searchHit)
+                .forEach(hit -> cut.collect(hit, counter.incrementAndGet(), A_VERSION));
+
+        assertEquals(
+                5 / 15d,
+                cut.valueFactory(A_VERSION).value().doubleValue(),
+                0.01);
+    }
+}
