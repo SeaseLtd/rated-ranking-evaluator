1f8721b08f120117d7bef4ec375a119951ffa1fe
Matt Pearce <matt@flax.co.uk>
2019-02-21 11:52:30 +0000

    Provide external Solr connector, allowing connections to already-running Solr instances.
    Squashed commit of the following:
    
    commit d545ae6fb06a9e5cca48dce3a13c71465c914818
    Author: Matt Pearce <matt@flax.co.uk>
    Date:   Thu Feb 21 11:25:02 2019 +0000
    
        Add socket and connection timeout config for Solr clients.
    
    commit 2dd0b4d3477dbd5862703dc12323446d8d452747
    Author: Matt Pearce <matt@flax.co.uk>
    Date:   Thu Feb 21 09:48:12 2019 +0000
    
        Tidy pom.xml
    
    commit 8867871f20a195f297bf050d00b9e0d57e55652d
    Author: Matt Pearce <matt@flax.co.uk>
    Date:   Thu Feb 21 09:37:45 2019 +0000
    
        Implement usage of CloudSolrClient with multiple Solr instances.
    
        - Modify SolrSettings to allow for multiple base URLs, Zookeeper hosts.
        - Implement external manager class for instantiating SolrClients.
        - Modify archetype Solr settings files to reflect changes.
    
    commit 369a2dbe5f0a4ee090eb029ab87878c9e4101525
    Author: Matt Pearce <matt@flax.co.uk>
    Date:   Wed Feb 20 10:58:13 2019 +0000
    
        Add external Solr archetype.
    
    commit 94f6a177804753cb5de437a7e207b789d4340fa1
    Author: Matt Pearce <matt@flax.co.uk>
    Date:   Wed Feb 20 10:09:56 2019 +0000
    
        Tidy module renaming - fix unchanged pom files.
    
    commit bb5ddc6d726a0e4cb72aa2b0350f3c8e5f9e610f
    Author: Matt Pearce <matt@flax.co.uk>
    Date:   Wed Feb 20 09:52:48 2019 +0000
    
        Rename external Solr implementation directory for consistency.
    
    commit f7ed7b2e8f05575398d5dafea0f4bdb7f6b667f3
    Author: Matt Pearce <matt@flax.co.uk>
    Date:   Wed Feb 20 09:33:14 2019 +0000
    
        Initial work to add external Solr access to RRE evaluation.
    
        - Added module to search platform.
        - Added module to Maven plugins.

---
+++ rre-maven-plugin/rre-maven-external-solr-plugin/src/main/java/io/sease/rre/maven/plugin/externalsolr/RREvaluateMojo.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sease.rre.maven.plugin.externalsolr;
+
+import io.sease.rre.core.Engine;
+import io.sease.rre.core.domain.metrics.MetricClassManager;
+import io.sease.rre.core.domain.metrics.ParameterizedMetricClassManager;
+import io.sease.rre.core.domain.metrics.SimpleMetricClassManager;
+import io.sease.rre.persistence.PersistenceConfiguration;
+import io.sease.rre.search.api.SearchPlatform;
+import io.sease.rre.search.api.impl.ExternalApacheSolr;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * RREvalutation Mojo (External Apache Solr binding).
+ *
+ * @author Matt Pearce (matt@flax.co.uk)
+ */
+@Mojo(name = "evaluate", inheritByDefault = false, defaultPhase = LifecyclePhase.PACKAGE)
+public class RREvaluateMojo extends AbstractMojo {
+
+    @Parameter(name = "configurations-folder", defaultValue = "${basedir}/src/etc/configuration_sets")
+    private String configurationsFolder;
+
+    @Parameter(name = "ratings-folder", defaultValue = "${basedir}/src/etc/ratings)")
+    private String ratingsFolder;
+
+    @Parameter(name = "templates-folder", defaultValue = "${basedir}/src/etc/templates")
+    private String templatesFolder;
+
+    @Parameter(name = "metrics", defaultValue = "io.sease.rre.core.domain.metrics.impl.PrecisionAtOne,io.sease.rre.core.domain.metrics.impl.PrecisionAtTwo,io.sease.rre.core.domain.metrics.impl.PrecisionAtThree,io.sease.rre.core.domain.metrics.impl.PrecisionAtTen")
+    private List<String> metrics;
+
+    @Parameter(name = "parameterizedMetrics")
+    private Map<String, Map> parameterizedMetrics;
+
+    @Parameter(name = "fields", defaultValue = "*,score")
+    private String fields;
+
+    @Parameter(name = "include")
+    private List<String> include;
+
+    @Parameter(name = "exclude")
+    private List<String> exclude;
+
+    @Parameter(name = "persistence")
+    private PersistenceConfiguration persistence = PersistenceConfiguration.DEFAULT_CONFIG;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        try (final SearchPlatform platform = new ExternalApacheSolr()) {
+            final MetricClassManager metricClassManager =
+                    parameterizedMetrics == null ? new SimpleMetricClassManager(metrics) : new ParameterizedMetricClassManager(metrics, parameterizedMetrics);
+            final Engine engine = new Engine(
+                    platform,
+                    configurationsFolder,
+                    null,
+                    ratingsFolder,
+                    templatesFolder,
+                    metricClassManager,
+                    fields.split(","),
+                    exclude,
+                    include,
+                    null,
+                    persistence);
+
+            final Map<String, Object> configuration = Collections.emptyMap();
+
+            engine.evaluate(configuration);
+        } catch (final IOException exception) {
+            throw new MojoExecutionException(exception.getMessage(), exception);
+        }
+    }
+}
