fabd4bc092d07d214e070bf04e181cd464221138
Matt Pearce <matt@elysiansoftware.co.uk>
2020-11-23 15:21:59 +0000

    Fix indents for consistency.

--- rre-search-platform/rre-search-platform-elastic-search-impl/src/test/java/io/sease/rre/search/api/impl/ExternalElasticSearchIT.java
+++ rre-search-platform/rre-search-platform-elastic-search-impl/src/test/java/io/sease/rre/search/api/impl/ExternalElasticSearchIT.java
@@ -54,157 +54,157 @@ import static org.junit.Assert.assertTrue;
  */
 public class ExternalElasticSearchIT {
 
-	private static final String ELASTICSEARCH_CONTAINER_BASE = "docker.elastic.co/elasticsearch/elasticsearch";
-	private static final String DEFAULT_ELASTICSEARCH_VERSION = "7.5.0";
+    private static final String ELASTICSEARCH_CONTAINER_BASE = "docker.elastic.co/elasticsearch/elasticsearch";
+    private static final String DEFAULT_ELASTICSEARCH_VERSION = "7.5.0";
 
-	private static final String INDEX_NAME = "test";
-	private static final String INDEX_VERSION = "v1.0";
+    private static final String INDEX_NAME = "test";
+    private static final String INDEX_VERSION = "v1.0";
 
-	private static final DockerImageName DOCKER_IMAGE = DockerImageName.parse(ELASTICSEARCH_CONTAINER_BASE + ":" + System.getProperty("elasticsearch.version", DEFAULT_ELASTICSEARCH_VERSION));
+    private static final DockerImageName DOCKER_IMAGE = DockerImageName.parse(ELASTICSEARCH_CONTAINER_BASE + ":" + System.getProperty("elasticsearch.version", DEFAULT_ELASTICSEARCH_VERSION));
 
-	@Rule
-	public TemporaryFolder tempFolder = new TemporaryFolder();
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
 
-	private SearchPlatform platform;
+    private SearchPlatform platform;
 
-	@Before
-	public void setupPlatform() {
-		platform = new ExternalElasticsearch();
-	}
+    @Before
+    public void setupPlatform() {
+        platform = new ExternalElasticsearch();
+    }
 
-	@Test
-	public void checkCollection_returnsFalseWhenNotLoaded() throws Exception {
-		final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
-		es.start();
+    @Test
+    public void checkCollection_returnsFalseWhenNotLoaded() throws Exception {
+        final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
+        es.start();
 
-		final File settingsFile = tempFolder.newFile("ccrf_settings.json");
-		FileWriter fw = new FileWriter(settingsFile);
-		fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ]}");
-		fw.close();
+        final File settingsFile = tempFolder.newFile("ccrf_settings.json");
+        FileWriter fw = new FileWriter(settingsFile);
+        fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ]}");
+        fw.close();
 
-		platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
+        platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
 
-		assertFalse(platform.checkCollection(INDEX_NAME, INDEX_VERSION));
-		es.close();
-	}
+        assertFalse(platform.checkCollection(INDEX_NAME, INDEX_VERSION));
+        es.close();
+    }
 
-	@Test
-	public void checkCollection_returnsTrueWhenAvailable() throws Exception {
-		final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
-		es.start();
+    @Test
+    public void checkCollection_returnsTrueWhenAvailable() throws Exception {
+        final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
+        es.start();
 
-		// Create an index
-		final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
-		hlClient.indices().create(new CreateIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
+        // Create an index
+        final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
+        hlClient.indices().create(new CreateIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
 
-		final File settingsFile = tempFolder.newFile("ccrt_settings.json");
-		FileWriter fw = new FileWriter(settingsFile);
-		fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ]}");
-		fw.close();
+        final File settingsFile = tempFolder.newFile("ccrt_settings.json");
+        FileWriter fw = new FileWriter(settingsFile);
+        fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ]}");
+        fw.close();
 
-		platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
+        platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
 
-		assertTrue(platform.checkCollection(INDEX_NAME, INDEX_VERSION));
-		hlClient.close();
-		es.close();
-	}
+        assertTrue(platform.checkCollection(INDEX_NAME, INDEX_VERSION));
+        hlClient.close();
+        es.close();
+    }
 
-	@Test
-	public void checkCollection_allowsIndexOverride() throws Exception {
-		final String overrideIndex = "override";
+    @Test
+    public void checkCollection_allowsIndexOverride() throws Exception {
+        final String overrideIndex = "override";
 
-		final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
-		es.start();
+        final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
+        es.start();
 
-		// Create an index
-		final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
-		hlClient.indices().create(new CreateIndexRequest(overrideIndex), RequestOptions.DEFAULT);
+        // Create an index
+        final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
+        hlClient.indices().create(new CreateIndexRequest(overrideIndex), RequestOptions.DEFAULT);
 
-		final File settingsFile = tempFolder.newFile("ccaio_settings.json");
-		FileWriter fw = new FileWriter(settingsFile);
-		fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ], \"index\": \"" + overrideIndex + "\" }");
-		fw.close();
+        final File settingsFile = tempFolder.newFile("ccaio_settings.json");
+        FileWriter fw = new FileWriter(settingsFile);
+        fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ], \"index\": \"" + overrideIndex + "\" }");
+        fw.close();
 
-		platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
+        platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
 
-		assertTrue(platform.checkCollection(INDEX_NAME, INDEX_VERSION));
-		hlClient.close();
-		es.close();
-	}
+        assertTrue(platform.checkCollection(INDEX_NAME, INDEX_VERSION));
+        hlClient.close();
+        es.close();
+    }
 
 
-	@Test
-	public void executeQuery_returnsResults() throws Exception {
-		final String overrideIndex = "override";
+    @Test
+    public void executeQuery_returnsResults() throws Exception {
+        final String overrideIndex = "override";
 
-		final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
-		es.start();
+        final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
+        es.start();
 
-		// Create the default index
-		final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
-		hlClient.indices().create(new CreateIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
+        // Create the default index
+        final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
+        hlClient.indices().create(new CreateIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
 
-		// Add some docs to the index
-		final Map<String, Object> source = new HashMap<>();
-		source.put("title", "Test");
-		hlClient.index(new IndexRequest(INDEX_NAME).id("1").source(source), RequestOptions.DEFAULT);
-		hlClient.index(new IndexRequest(INDEX_NAME).id("2").source(source), RequestOptions.DEFAULT);
-		hlClient.index(
-				new IndexRequest(INDEX_NAME).id("3").source(source).setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE),
-				RequestOptions.DEFAULT);
+        // Add some docs to the index
+        final Map<String, Object> source = new HashMap<>();
+        source.put("title", "Test");
+        hlClient.index(new IndexRequest(INDEX_NAME).id("1").source(source), RequestOptions.DEFAULT);
+        hlClient.index(new IndexRequest(INDEX_NAME).id("2").source(source), RequestOptions.DEFAULT);
+        hlClient.index(
+                new IndexRequest(INDEX_NAME).id("3").source(source).setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE),
+                RequestOptions.DEFAULT);
 
-		final File settingsFile = tempFolder.newFile("eq_settings.json");
-		FileWriter fw = new FileWriter(settingsFile);
-		fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ]}");
-		fw.close();
+        final File settingsFile = tempFolder.newFile("eq_settings.json");
+        FileWriter fw = new FileWriter(settingsFile);
+        fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ]}");
+        fw.close();
 
-		platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
+        platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
 
-		final QueryOrSearchResponse response = platform.executeQuery(INDEX_NAME, INDEX_VERSION, "{ \"query\": { \"match_all\": {} }}", source.keySet().toArray(new String[0]), 10);
+        final QueryOrSearchResponse response = platform.executeQuery(INDEX_NAME, INDEX_VERSION, "{ \"query\": { \"match_all\": {} }}", source.keySet().toArray(new String[0]), 10);
 
-		assertEquals(3, response.totalHits());
+        assertEquals(3, response.totalHits());
 
-		hlClient.close();
-		es.close();
-	}
+        hlClient.close();
+        es.close();
+    }
 
-	@Test
-	public void executeQuery_allowsIndexOverride() throws Exception {
-		final String overrideIndex = "override";
+    @Test
+    public void executeQuery_allowsIndexOverride() throws Exception {
+        final String overrideIndex = "override";
 
-		final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
-		es.start();
+        final ElasticsearchContainer es = new ElasticsearchContainer(DOCKER_IMAGE);
+        es.start();
 
-		// Create the default index
-		final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
-		hlClient.indices().create(new CreateIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
+        // Create the default index
+        final RestHighLevelClient hlClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(es.getHttpHostAddress())));
+        hlClient.indices().create(new CreateIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
 
-		// Create an override index
-		hlClient.indices().create(new CreateIndexRequest(overrideIndex), RequestOptions.DEFAULT);
+        // Create an override index
+        hlClient.indices().create(new CreateIndexRequest(overrideIndex), RequestOptions.DEFAULT);
 
-		// Add some docs to the base index
-		final Map<String, Object> source = new HashMap<>();
-		source.put("title", "Test");
-		hlClient.index(new IndexRequest(INDEX_NAME).id("1").source(source), RequestOptions.DEFAULT);
+        // Add some docs to the base index
+        final Map<String, Object> source = new HashMap<>();
+        source.put("title", "Test");
+        hlClient.index(new IndexRequest(INDEX_NAME).id("1").source(source), RequestOptions.DEFAULT);
 
-		// Add a different number of docs to the override index
-		hlClient.index(new IndexRequest(overrideIndex).id("1").source(source), RequestOptions.DEFAULT);
-		hlClient.index(
-				new IndexRequest(overrideIndex).id("2").source(source).setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE),
-				RequestOptions.DEFAULT);
+        // Add a different number of docs to the override index
+        hlClient.index(new IndexRequest(overrideIndex).id("1").source(source), RequestOptions.DEFAULT);
+        hlClient.index(
+                new IndexRequest(overrideIndex).id("2").source(source).setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE),
+                RequestOptions.DEFAULT);
 
-		final File settingsFile = tempFolder.newFile("eqaio_settings.json");
-		FileWriter fw = new FileWriter(settingsFile);
-		fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ], \"index\": \"" + overrideIndex + "\" }");
-		fw.close();
+        final File settingsFile = tempFolder.newFile("eqaio_settings.json");
+        FileWriter fw = new FileWriter(settingsFile);
+        fw.write("{ \"hostUrls\": [ \"" + es.getHttpHostAddress() + "\" ], \"index\": \"" + overrideIndex + "\" }");
+        fw.close();
 
-		platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
+        platform.load(null, settingsFile, INDEX_NAME, INDEX_VERSION);
 
-		final QueryOrSearchResponse response = platform.executeQuery(INDEX_NAME, INDEX_VERSION, "{ \"query\": { \"match_all\": {} }}", source.keySet().toArray(new String[0]), 10);
+        final QueryOrSearchResponse response = platform.executeQuery(INDEX_NAME, INDEX_VERSION, "{ \"query\": { \"match_all\": {} }}", source.keySet().toArray(new String[0]), 10);
 
-		assertEquals(2, response.totalHits());
+        assertEquals(2, response.totalHits());
 
-		hlClient.close();
-		es.close();
-	}
+        hlClient.close();
+        es.close();
+    }
 }
