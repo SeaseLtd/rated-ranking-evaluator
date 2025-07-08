eb310cde56fc48d3a2053c552557671e5bca649c
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-07-08 17:55:18 +0200

    Addressed comments [PR#150](https://github.com/SeaseLtd/rated-ranking-evaluator/pull/150/files/d3a000e9cf6ae284ac03a469e53ff5d38ee9cc4d)
    ## Changes
    - removed file uv.lock and add it in .gitignore
    - simplified logger.py
    - added logs/ folder
    - removed Makefile
    - resolved conflicts in pyproject.toml
    - simplified test file test_dataset_generation_dummy.py

--- rre-dataset-generator/tests/unit/test_dataset_generation_dummy.py
+++ rre-dataset-generator/tests/unit/test_dataset_generation_dummy.py
@@ -1,14 +1,8 @@
 import sys
 import os
-import logging
 from src.logger import configure_logging
 import pytest
-# from typer.testing import CliRunner
 from dataset_generator import parse_args
-
-# Add project root to Python path
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(0, project_root)
 
 
 # Configure logging as soon as the module is loaded
@@ -17,24 +11,6 @@ configure_logging()
 @pytest.fixture
 def args():
     return parse_args()
-
-# def test_argparse_yaml_with_list(caplog):
-#     # Set up logging capture
-#     caplog.set_level(logging.INFO)
-#
-#     runner = CliRunner()
-#     # Pass config_path as a positional argument
-#     result = runner.invoke(app, ["src/yaml_configs/config.yml"])
-#
-#     # Print the logs for debugging
-#     for record in caplog.records:
-#         print(f"LOG: {record.message}")
-#
-#     # Check if the log message appears in any of the log records
-#     assert any("Starting dataset generation process..." in record.message
-#               for record in caplog.records)
-#     assert result.exit_code == 0
-
 
 def test_argparse_with_defaults(args):
     assert args.dummy == "default"
