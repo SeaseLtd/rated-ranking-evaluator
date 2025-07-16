1b23f8d69886d9a947d9cb9e8c682b84fbc84ce6
MrCabss69 <p.p.cabezon@proton.me>
2025-07-16 19:01:20 +0200

    feat: Implement AbstractWriter and QuepidWriter for data export

---
+++ rre-dataset-generator/src/utils.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from src.search_engine.data_store import DataStore
+import csv
+
+class AbstractWriter(ABC):
+    """
+    Abstract base class for writers.
+    
+    The writer has to read the data structure and export it to a format (e.g.:quepid, rre..)
+    """
+    def __init__(self, datastore: DataStore):
+        self.datastore = datastore
+
+    @abstractmethod
+    def write(self, output_path: str) -> None:
+        """Writes the data from the datastore to a file."""
+        pass
+    
+
+class QuepidWriter(AbstractWriter):
+    """
+    QuepidWriter: Write the data structure to a Quepid format (CSV).
+    """
+    def write(self, output_path: str) -> None:
+        """
+        Writes queries and their scored documents to a CSV file in Quepid format.
+        The format is: query,docid,rating
+        """
+        queries = self.datastore.get_queries() # Query List
+        
+        with open(output_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['query', 'docid', 'rating'])
+            
+            for query in queries:
+                for score in query.scores:
+                    if score.value != -1:  # Only write rated documents
+                        writer.writerow([query.text, score.doc_id, score.value])
