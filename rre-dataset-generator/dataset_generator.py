7917fbdc0decc9dc75cfb102c61e1db717036c50
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-07-16 11:04:56 +0200

    [DAGE-3] removed try_solr.py file to have all the code in the main file

--- rre-dataset-generator/dataset_generator.py
+++ rre-dataset-generator/dataset_generator.py
@@ -1,8 +1,21 @@
 from src.config import Config
 from src.utils import parse_args
+import json
+
+from src.search_engine.solr_search_engine import SolrSearchEngine
 
 if __name__ == "__main__":
     args = parse_args()
 
     config = Config.load(args.config_file)
 
+    search_engine = SolrSearchEngine('http://localhost:8983/solr/testcore/')
+
+    # docs = search_engine.fetch_for_query_generation(documents_filter=config.documents_filter,
+    #                                                            doc_number=config.doc_number)
+
+    docs = search_engine.fetch_for_evaluation(keyword="and", query_template=config.query_template)
+
+    with open("solr_extracted_dataset.json", "w") as file:
+        json.dump(docs, file, indent=4)
+        file.write('\n')
