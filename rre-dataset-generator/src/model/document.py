de0456ef026f444d748cc28b06ac08296a27350f
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-07-16 18:25:05 +0200

    Added --verbose param to parse_args function
    ## Changes
    - modified the configure_logging to be called just one time from main file
    - removed the same call in other files

--- rre-dataset-generator/src/model/document.py
+++ rre-dataset-generator/src/model/document.py
@@ -1,5 +1,8 @@
 from typing import Dict, Any
 from pydantic import BaseModel, Field, field_validator, ValidationError
+import logging
+
+log = logging.getLogger(__name__)
 
 class Document(BaseModel):
     """
@@ -20,7 +23,9 @@ class Document(BaseModel):
     def check_no_empty_fields(cls, v: Dict[str, Any]) -> Dict[str, Any]:
         """Validate that the fields dictionary is not empty and its keys are not empty."""
         if not v:
+            log.error('The fields dictionary cannot be empty.')
             raise ValueError('The fields dictionary cannot be empty.')
         if any(not key for key in v.keys()):
+            log.error('Field keys cannot be empty strings.')
             raise ValueError('Field keys cannot be empty strings.')
         return v
