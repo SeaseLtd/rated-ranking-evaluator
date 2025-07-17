605d3bec9a8f723a690971266025e68fc37b8a24
Nazerke Seidan <n.seidan@sease.io>
2025-07-17 17:50:14 +0500

    DAGE-11: Add generate_queries in LLM service

--- rre-dataset-generator/src/llm/llm_service.py
+++ rre-dataset-generator/src/llm/llm_service.py
@@ -1,23 +1,46 @@
+import json
+import logging
+from json import JSONDecodeError
+from typing import List, Union
+
 from langchain_core.language_models import BaseChatModel
 from langchain_core.messages import HumanMessage, SystemMessage
+
+from src.logger import configure_logging
+from src.model.document import Document
+
+configure_logging(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 class LLMService:
     def __init__(self, chat_model: BaseChatModel):
         self.chat_model = chat_model
 
-    def test_connection(self, prompt: str) -> str:
+    def generate_queries(self, document: Document, num_queries_generate_per_doc: int) -> Union[List[str], str]:
         """
-        Send a test prompt to verify the model returns a response.
+        Generate queries based on the given document and num_queries_generate_per_doc and
+        Returns a list of generated queries or just a generated string in case of LLM hallucination
         """
+        system_prompt = (
+            f"You are a helpful assistant! Generate {num_queries_generate_per_doc} "
+            "keyword-phrase-based queries based on the given document below. "
+            "**Output only** a JSON array of strings—nothing else. "
+            "Example format: [\"first query\", \"second query\"]"
+        )
+
+        doc_json = document.model_dump_json()
+
         messages = [
-            SystemMessage(
-                content="You are a helpful assistant!"
-            ),
-            HumanMessage(
-                content=prompt
-            )
+            SystemMessage(content=system_prompt),
+            HumanMessage(content=f"Document:\n{doc_json}")
         ]
 
-        response = self.chat_model.invoke(messages)
-        return response.content.strip()
+        raw = self.chat_model.invoke(messages).content.strip()
+
+        # in case LLM hallucinates
+        try:
+            return json.loads(raw)
+        except JSONDecodeError:
+            log.warning("LLM hallucinated and its response: %s", raw)
+            return raw
--- rre-dataset-generator/tests/unit/test_llm_service.py
+++ rre-dataset-generator/tests/unit/test_llm_service.py
@@ -1,12 +1,21 @@
 from langchain_core.language_models.fake_chat_models import FakeListChatModel
 from src.llm.llm_service import LLMService
+from src.model.document import Document
 
 
 def test_llm_service_test_connection():
-    fake_llm = FakeListChatModel(responses=["Hello, world!"])
+    fake_llm = FakeListChatModel(responses=["Car"])
     service = LLMService(chat_model=fake_llm)
 
-    response = service.test_connection("Say hello world!")
+    doc1 = Document(
+        id="doc1",
+        fields={
+            "title": "Car of the Year",
+            "description": "The Toyota Camry, the nation's most popular car has now been rated as its best new model."
+        }
+    )
+
+    response = service.generate_queries(doc1, 5)
     assert isinstance(response, str)
-    assert "Hello, world!" in response
+    assert "Car" in response
 
