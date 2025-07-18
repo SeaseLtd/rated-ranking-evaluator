ccd137b114ca4fdd2b672b6ce82c5a85c8a2e180
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-07-18 08:42:46 +0200

    [DAGE-15] updated dataset_generator.py

--- rre-dataset-generator/src/utils.py
+++ rre-dataset-generator/src/utils.py
@@ -1,6 +1,8 @@
 import argparse
 import re
 import html
+from pathlib import Path
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Parse arguments for CLI.')
@@ -15,3 +17,11 @@ def clean_text(text):
     tag_regex = re.compile('<.*?>')
     text_without_html = re.sub(tag_regex, '', text)
     return html.unescape(re.sub("\s\s+", " ", text_without_html))
+
+def count_non_empty_lines(file_path: Path) -> int:
+    count = 0
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            if line.strip():  # Strip removes whitespace; if anything remains, it's a non-empty line
+                count += 1
+    return count
