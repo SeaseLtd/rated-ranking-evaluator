87072f2c48a250e43bbbd14b778547f92e6e0adb
MrCabss69 <p.p.cabezon@proton.me>
2025-07-23 12:14:27 +0200

    Updated Pydantic Schemas for LLMQueryReponse - Needed to update the LMService basic tests to adopt JSON format; and added a custom test file. Also: removed comments as feedback was asking for

--- rre-dataset-generator/tests/unit/test_llm_service.py
+++ rre-dataset-generator/tests/unit/test_llm_service.py
@@ -1,10 +1,13 @@
 from langchain_core.language_models.fake_chat_models import FakeListChatModel
 from src.llm.llm_service import LLMService
 from src.model.document import Document
+from src.model.llm_schemas import LLMQueryResponse
 
 
 def test_llm_service_test_connection():
-    fake_llm = FakeListChatModel(responses=["Car"])
+    ## UPDATED TESTS TO ADOPT THE NEW RESPONSE FORMAT - LLMQueryResponse
+    # WARNING: now the responses should be JSON strings!
+    fake_llm = FakeListChatModel(responses=['["Car"]'])
     service = LLMService(chat_model=fake_llm)
 
     doc1 = Document(
@@ -16,6 +19,7 @@ def test_llm_service_test_connection():
     )
 
     response = service.generate_queries(doc1, 5)
-    assert isinstance(response, str)
-    assert "Car" in response
 
+    assert isinstance(response, LLMQueryResponse)
+    assert response.content_list == ["Car"]
+    assert response.content == '["Car"]'
