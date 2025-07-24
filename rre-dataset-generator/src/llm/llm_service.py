89be81754ec4dd0d7ca189d8ae57589555e33e0b
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-07-24 16:52:34 +0200

    [DAGE-25] updated factories and other minor changes mentioned in the PR

--- rre-dataset-generator/src/llm/llm_service.py
+++ rre-dataset-generator/src/llm/llm_service.py
@@ -23,7 +23,7 @@ class LLMService:
         """
         system_prompt = (
             f"You are a helpful assistant! Generate {num_queries_generate_per_doc} "
-            "queries based on the given document below. " #keyword-phrase-based
+            "queries based on the given document below. "
             "**Output only** a JSON array of strings—nothing else. "
             "Example format: [\"first query\", \"second query\"]"
         )
