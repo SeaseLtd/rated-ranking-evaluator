ce70b600dc377e2ee72e9304d806433823e2db8d
Intrinsical-AI <intrinsicalai@proton.me>
2025-08-20 13:21:17 +0200

    Refined and polished

--- rre-dataset-generator/src/search_engine/__init__.py
+++ rre-dataset-generator/src/search_engine/__init__.py
@@ -2,3 +2,4 @@ from .search_engine_factory import SearchEngineFactory
 from .opensearch_engine import OpenSearchEngine
 from .solr_search_engine import SolrSearchEngine
 from .elasticsearch_search_engine import ElasticsearchSearchEngine
+from .search_engine_base import BaseSearchEngine
