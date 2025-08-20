618476211f5eda179a2b237bf76dc9397b1b219c
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-08-20 12:22:09 +0200

    [DAGE-31] changes to vector embedding dim in solr-init.sh Now the vector field is created only when needed and the embedding dimension is detected from the first line of the doc_embedding.jsonl file

--- rre-dataset-generator/tests/integration/solr-init/solr-init.sh
+++ rre-dataset-generator/tests/integration/solr-init/solr-init.sh
@@ -6,7 +6,7 @@ DATASET_FILE="/opt/rre-dataset-generator/data/dataset.json";
 EMBEDDINGS_FILE="/opt/rre-dataset-generator/embeddings/doc_embeddings.jsonl";
 TMP_FILE="/tmp/merged_dataset.json";
 
-echo "[INFO] Waiting for Solr core '\''testcore'\'' to be ready…";
+echo "[INFO] Waiting for Solr core \"testcore\" to be ready…";
 max=30;
 for i in $(seq 1 $max); do
   if curl -sf $COLLECTION_ENDPOINT/admin/ping?wt=json > /dev/null; then
@@ -23,34 +23,36 @@ if ! curl -sf $COLLECTION_ENDPOINT/admin/ping?wt=json > /dev/null; then
   exit 1;
 fi
 
-curl -f -X POST -H 'Content-type:application/json' --data-binary '{
-  "add-field-type" : {
-      "name":"knn_vector_10",
-      "class":"solr.DenseVectorField",
-      "vectorDimension":10,
-      "similarityFunction":"cosine",
-      "knnAlgorithm":"hnsw"
-    },
-  "add-field" : {
-      "name":"vector",
-      "type":"knn_vector_10",
-      "indexed":true,
-      "stored":true
-    }
-  }' "$COLLECTION_ENDPOINT/schema"
-
 SOLR_RESPONSE=$(curl -sf $COLLECTION_ENDPOINT/select\?q\=\*:\*)
 COUNT_FOUND=$(echo "$SOLR_RESPONSE" | { grep -cE '"numFound":[1-9]+' || test $? = 1; })
 echo "num found: $COUNT_FOUND"
 
 if [ -f "$EMBEDDINGS_FILE" ] && command -v jq >/dev/null 2>&1; then
-  echo "[INFO] Embeddings file found and jq available → merging…"
+  VECTOR_DIM=$(head -n 1 "$EMBEDDINGS_FILE" | jq '.vector | length')
+  echo "[INFO] Embeddings file found -> detected embedding dimension: $VECTOR_DIM"
+  echo "[INFO] jq available → merging…"
   jq --slurpfile emb "$EMBEDDINGS_FILE" '
     map(. as $d | $emb[] | select(.id == $d.id) | $d + {vector: .vector}) as $with_vec
     | map(if ($with_vec[]? | select(.id == .id) | .id) == .id
           then ($with_vec[] | select(.id == .id))
           else . end)
   ' "$DATASET_FILE" > "$TMP_FILE"
+  echo "[INFO] Updating solr collection with vector field (dim=$VECTOR_DIM)…"
+  curl -f -X POST -H 'Content-type:application/json' --data-binary "{
+  \"add-field-type\" : {
+      \"name\":\"knn_vector\",
+      \"class\":\"solr.DenseVectorField\",
+      \"vectorDimension\":$VECTOR_DIM,
+      \"similarityFunction\":\"cosine\",
+      \"knnAlgorithm\":\"hnsw\"
+    },
+  \"add-field\" : {
+      \"name\":\"vector\",
+      \"type\":\"knn_vector\",
+      \"indexed\":true,
+      \"stored\":true
+    }
+  }" $COLLECTION_ENDPOINT/schema
 else
   echo "[INFO] Using plain dataset (no embeddings or jq missing)…"
   cp "$DATASET_FILE" "$TMP_FILE"
