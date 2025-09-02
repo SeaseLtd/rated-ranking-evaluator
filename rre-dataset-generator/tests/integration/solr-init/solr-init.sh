1e88a5460522ba6a072b4f61bedf71f132035415
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-09-02 13:17:38 +0200

    [DAGE-31] Updated Dockerfile and solr-init.sh with commented jq code + folder creation

--- rre-dataset-generator/tests/integration/solr-init/solr-init.sh
+++ rre-dataset-generator/tests/integration/solr-init/solr-init.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 COLLECTION_ENDPOINT="http://solr:8983/solr/testcore";
 DATASET_FILE="/opt/rre-dataset-generator/data/dataset.json";
-EMBEDDINGS_FILE="/opt/rre-dataset-generator/embeddings/doc_embeddings.jsonl";
+EMBEDDINGS_FOLDER="/opt/rre-dataset-generator/embeddings";
+EMBEDDINGS_FILE="$EMBEDDINGS_FOLDER/doc_embeddings.jsonl";
 TMP_FILE="/tmp/merged_dataset.json";
 
 echo "[INFO] Waiting for Solr core \"testcore\" to be ready…";
@@ -27,16 +28,22 @@ SOLR_RESPONSE=$(curl -sf $COLLECTION_ENDPOINT/select\?q\=\*:\*)
 COUNT_FOUND=$(echo "$SOLR_RESPONSE" | { grep -cE '"numFound":[1-9]+' || test $? = 1; })
 echo "num found: $COUNT_FOUND"
 
+mkdir -p $EMBEDDINGS_FOLDER
+
 if [ -f "$EMBEDDINGS_FILE" ] && command -v jq >/dev/null 2>&1; then
   VECTOR_DIM=$(head -n 1 "$EMBEDDINGS_FILE" | jq '.vector | length')
   echo "[INFO] Embeddings file found -> detected embedding dimension: $VECTOR_DIM"
   echo "[INFO] jq available → merging…"
-  jq --slurpfile emb "$EMBEDDINGS_FILE" '
-    map(. as $d | $emb[] | select(.id == $d.id) | $d + {vector: .vector}) as $with_vec
-    | map(if ($with_vec[]? | select(.id == .id) | .id) == .id
-          then ($with_vec[] | select(.id == .id))
-          else . end)
-  ' "$DATASET_FILE" > "$TMP_FILE"
+  jq --slurpfile emb "$EMBEDDINGS_FILE" '       # parse the $EMBEDDINGS_FILE into the var `emb` and execute the following script
+  map(
+    . as $doc                                   # save each line of the document in the var `doc`
+    | ($emb[] | select(.id == $doc.id)) as $e   # assign to the var `e` the line of the emb file if it has the same id
+    | if $e != null
+      then $doc + {vector: $e.vector}           # appends the embedding to the document
+      else $doc                                 # otherwise it keeps just the fields
+      end
+  )
+' "$DATASET_FILE" > "$TMP_FILE"
   echo "[INFO] Updating solr collection with vector field (dim=$VECTOR_DIM)…"
   curl -f -X POST -H 'Content-type:application/json' --data-binary "{
   \"add-field-type\" : {
