20faba1e5c5c6b39b1d59845540a30be0824a3c4
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-09-09 14:57:11 +0200

    [DAGE-64] fixed path for main script (still not working)

--- rre-tools/dataset-generator/src/dataset_generator/llm/__init__.py
+++ rre-tools/dataset-generator/src/dataset_generator/llm/__init__.py
@@ -1,6 +1,6 @@
-from .llm_config import LLMConfig
-from .llm_provider_factory import LLMServiceFactory
-from .llm_service import LLMService
+from dataset_generator.llm.llm_config import LLMConfig
+from dataset_generator.llm.llm_provider_factory import LLMServiceFactory
+from dataset_generator.llm.llm_service import LLMService
 
 __all__ = [
     "LLMConfig",
---
+++ rre-tools/embedding-model-evaluator/src/embedding_model_evaluator/custom_tasks/__init__.py
@@ -0,0 +1,7 @@
+from embedding_model_evaluator.custom_tasks.reranking_task import CustomRerankingTask
+from embedding_model_evaluator.custom_tasks.retrieval_task import CustomRetrievalTask
+
+__all__ = [
+    "CustomRerankingTask",
+    "CustomRetrievalTask"
+]
