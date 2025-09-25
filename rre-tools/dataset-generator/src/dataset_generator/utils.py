70fc8009290a5a3706dd50b0d564e11891cbf9f7
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-09-25 17:41:17 +0200

    [DAGE-84] Fixed READMEs and

--- rre-tools/dataset-generator/src/dataset_generator/utils.py
+++ rre-tools/dataset-generator/src/dataset_generator/utils.py
@@ -9,8 +9,8 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Parse arguments for CLI.')
 
     parser.add_argument('-c', '--config_file', type=str,
-                        help='Config file path to use for the application [default: \"config.yaml\"]',
-                        required=False, default="config.yaml")
+                        help='Config file path to use for the application [default: "dataset-generator/config.yaml"]',
+                        required=False, default="dataset-generator/config.yaml")
 
     parser.add_argument('-v', '--verbose',action='store_true',
                         help='Activate debug mode for logging [default: False]')
